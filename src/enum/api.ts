0a4fb88ca5820edea1e388fd6e1b493604e3a689
liu-shu-yu410638166 <410638166@gms.tku.edu.tw>
2024-12-19 23:18:49 +0800

    all

--- src/enum/api.ts
+++ src/enum/api.ts
@@ -1,3 +1,17 @@
-export enum api{
-    findAll = "http://163.13.201.151.:8877/api/v1/user/findAll"
-}
+import { useState, useEffect } from 'react';
+
+export const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch(() => setError('伺服器無回應，請檢查網路連線！'))
+      .finally(() => setLoading(false));
+  }, [url]);
+
+  return { data, loading, error };
+};
